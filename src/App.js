f04c671ff51413fdcf6714057cddd1602dc18bc3
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-19 18:46:12 +0700

    [PHAN-02] 006 Dinh nghia component cach so 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function One (){
+  return (
+    <div>
+      <h2>so 1 tap 1</h2>
+      <h3>so 1 tap 2</h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <One/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
