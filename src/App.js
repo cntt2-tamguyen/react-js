dd3c0a269a19923d1a987005649219c0b7ee9517
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-19 19:10:05 +0700

    [PHAN-04] 011 Cach viet jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const a1 = "Tam"
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="nut">Toi ten la: {a1}</h3>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" name id defaultValue="checkedValue" defaultChecked />
+            Display value
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <App1/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
