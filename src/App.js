0ebb7d34d6edd1d88bdcc68c98b10a7e8eea7614
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-19 19:51:36 +0700

    [PHAN-05] 016 Hoan thien giao dien

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import TopMenu from './Component/TopMenu/TopMenu';
 import Header from './Component/Header/Header';
+import Content from './Component/Content/Content';
 
 function App() {
   return (
     <div className="_013">
       <TopMenu/>
       <Header/>
+      <Content tieude="Tin tuc so 1" vitri1="order-lg-2" anh="img/01.jpg" trichdan="day la trci dan so 1"/>
+      <Content tieude="Tin tuc so 2" anh="img/02.jpg" trichdan="day la trci dan so 2"/>
+      <Content tieude="Tin tuc so 3" vitri1="order-lg-2" anh="img/03.jpg" trichdan="day la trci dan so 3"/>
     </div>
   );
 }
