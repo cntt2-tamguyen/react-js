1da559a61c1ae027ff9b3b81092951e8f98e4e04
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-19 18:54:49 +0700

    [PHAN-03] 009 Khai niem props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function NumberOne(props){
+  return (
+    <div className="col-6">
+      <div className="card">
+        <image className="card-img-top" src={props.linkanh} alt=""/>
+        <div>
+          <h4 className="card-title">{props.tieude}</h4>
+          <p className="card-text">Text</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +33,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="container">
+        <div className="row">
+          <NumberOne tieude="san pham 1" linkanh="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4PuS9SBFBVkLGJam0-CuSlu2S2BOPTkYIGfeIPnAK-eZ-oh6u"/>
+          <NumberOne tieude="san pham 2" linkanh="https://trithucvn.net/wp-content/uploads/2018/03/hoa-hong.jpeg"/>
+        </div>
+      </div>
     </div>
   );
 }
