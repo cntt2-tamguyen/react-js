bf420c243e62a8d76c99df91525afb7c3de8b56f
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-20 13:40:35 +0700

    [PHAN-09} 026 Su dung ham trong lifecycle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      trangthai:"khoitao"
+    }
+  }
+  
+  componentWillMount() {
+    console.log("componentWillMount da chay")
+  }
+  
+  componentDidMount() {
+    console.log("componentDidMount da chay")
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate da chay");
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate da chay")
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate da chay")
+  }
+  
+  capNhatState = () => {
+    this.setState({
+      trangthai:"trang thai duoc update"
+    });
+  }
+  render() {
+    console.log("render da chay");
+    console.log(this.state.trangthai);
+    return (
+      <div>
+        <button onClick={()=>this.capNhatState()}>Click de update state</button>
+      </div>
+    );
+  }
 }
 
 export default App;
