b2b9b3e9858cd7cb38882ec155f820d95d584adf
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-20 09:59:51 +0700

    [PHAN-07] 018 Cach viet tong quat tuong tac

--- src/Component/Content/Content.js
+++ src/Component/Content/Content.js
@@ -13,6 +13,11 @@ class Content extends Component {
     thongbao2 = () => {
         alert('Thong bao so 2')
     }
+
+    thongbao3 = (x) => {
+        alert(x)
+    }
+    
     render() {
         return (
             <section>
@@ -31,6 +36,8 @@ class Content extends Component {
                             <div className="btn btn-group">
                                 <div className="btn btn-info" onClick={this.thongbao}>Edit</div>
                                 <div className="btn btn-warning" onClick={this.thongbao2}>Remove</div>
+                                <div className="btn btn-warning" onClick={() => this.thongbao3("Thong bao 3") }>TB3</div>
+                                <div className="btn btn-warning" onClick={this.thongbao3.bind(this,"Thong bao 4") }>TB4</div>
                             </div>
                             <hr/>
                         </div>
