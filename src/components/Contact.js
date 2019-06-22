94e1c9a8fbba3c361b0d6ebc239b7c04cf8344ed
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-22 13:29:45 +0700

    [PHAN-17] 049 Xu ly phan the select

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,8 @@ class Contact extends Component {
     super(props);
     this.state = {
       isRedirect : false,
-      fName:"",
-      fEmail:"",
-      fPhone:"",
-      fMess:"",
-    }
+      fNgay:"thu7"
+      }
   }
   
   submitForm = (event) => {
@@ -34,6 +31,7 @@ class Contact extends Component {
     noiDung += " / Email nhan duoc la: " + this.state.fEmail;
     noiDung += " / Phone nhan duoc la: " + this.state.fPhone;
     noiDung += " / Message nhan duoc la: " + this.state.fMess;
+    noiDung += " / Ngay nhan duoc la: " + this.state.fNgay;
     return noiDung;
   }
 
@@ -98,6 +96,21 @@ class Contact extends Component {
                       <div className="form-group floating-label-form-group controls mb-0 pb-2">
                         <label>Message</label>
                         <textarea name="fMess" onChange={(event)=>this.isChange(event)} className="form-control" id="message" rows={5} placeholder="Message" required="required" data-validation-required-message="Please enter a message." defaultValue={""} />
+                        <p className="help-block text-danger" />
+                      </div>
+                    </div>
+                    <div className="control-group">
+                      <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                        <label>Chon ngay</label>
+                     
+                        
+                         <select value={this.state.fNgay} className="form-control" name="fNgay" onChange={(event)=>this.isChange(event)}>
+                           <option value="thu3">Ngày thứ 3</option>
+                           <option value="thu5"> Ngày thứ 5</option>
+                           <option value="thu7">Ngày thứ 7</option>
+                           <option value="chunhat">Ngày chủ nhật</option>
+
+                         </select>
                         <p className="help-block text-danger" />
                       </div>
                     </div>
