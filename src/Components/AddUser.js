dcdad2f0fec2589fcb771c24ee8fa0f94a1d0924
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-23 12:53:30 +0700

    [PHAN-25] 073 Day du lieu vao state

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class AddUser extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            id:"",
+            name:"",
+            phone:"",
+            permission:""
+        }
+    }
+    
     isChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        console.log(name);
-        console.log(value);
+        this.setState({
+            [name]:value
+        });
+        //packet to item
+        var item = [];
+        item.id=this.state.id;
+        item.name=this.state.name;
+        item.phone=this.state.phone;
+        item.permission=this.state.permission;
     }
 
     kiemTraTrangThai = () => {
