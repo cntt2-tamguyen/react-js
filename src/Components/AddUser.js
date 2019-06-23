109320b84ebd820d812122f2748f230aacf404bd
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-23 13:34:31 +0700

    [PHAN-26] 076 Xoa trang noi dung sau them moi

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -25,28 +25,30 @@ class AddUser extends Component {
         if (this.props.hienThiForm===true) {
             return (
                 <div className="col">
-                    <div className="card mb-3 mt-2" style={{maxWidth: '18rem'}}>
-                        <div className="card-header">Thêm mới user vào hệ thống</div>
-                        <div className="card-body">
-                            <div className="form-group">
-                                <input type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" name="name" onChange={(event)=>this.isChange(event)}/>
-                            </div>
-                            <div className="form-group">
-                                <input type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" name="phone" onChange={(event)=>this.isChange(event)}/>
-                            </div>
-                            <div className="form-group">
-                                <select className="custom-select" required name="permission" onChange={(event)=>this.isChange(event)}>
-                                    <option value>Chọn quyền mặc định</option>
-                                    <option value={1}>Admin</option>
-                                    <option value={2}>Moderator</option>
-                                    <option value={3}>Normal</option>
-                                </select>
-                            </div>
-                            <div className="form-group">
-                                <div className="btn btn-block btn-primary" onClick={(name,phone,permission)=>this.props.add(this.state.name,this.state.phone,this.state.permission)}>Thêm mới</div>
+                    <form>
+                        <div className="card mb-3 mt-2" style={{maxWidth: '18rem'}}>
+                            <div className="card-header">Thêm mới user vào hệ thống</div>
+                            <div className="card-body">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" name="name" onChange={(event)=>this.isChange(event)}/>
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" name="phone" onChange={(event)=>this.isChange(event)}/>
+                                </div>
+                                <div className="form-group">
+                                    <select className="custom-select" required name="permission" onChange={(event)=>this.isChange(event)}>
+                                        <option value>Chọn quyền mặc định</option>
+                                        <option value={1}>Admin</option>
+                                        <option value={2}>Moderator</option>
+                                        <option value={3}>Normal</option>
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <input type="reset" className="btn btn-block btn-primary" onClick={(name,phone,permission)=>this.props.add(this.state.name,this.state.phone,this.state.permission)} value="Thêm Mới"/>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             );
         }
