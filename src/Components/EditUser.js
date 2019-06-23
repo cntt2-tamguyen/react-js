0f2e907c84df402b88b39cf641d406ef03101d3c
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-24 00:10:03 +0700

    [PHAN-29] 084 Luu thong tin vao state

--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 class EditUser extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id:this.props.userEditOject.id,
+            name:this.props.userEditOject.name,
+            phone:this.props.userEditOject.phone,
+            permission:this.props.userEditOject.permission
+        }
+    }
+
+    isChange = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({[name]:value});
+    }
+
+    render() {        
         return (
             <div className="col">
                 <form>
@@ -9,13 +25,13 @@ class EditUser extends Component {
                         <div className="card-header text-center">Sửa thông tin User</div>
                         <div className="card-body">
                             <div className="form-group">
-                                <input defaultValue={this.props.userEditOject.name} type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" name="name" onChange={(event)=>this.isChange(event)}/>
+                                <input onChange={(event)=>this.isChange(event)} defaultValue={this.props.userEditOject.name} type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" name="name" onChange={(event)=>this.isChange(event)}/>
                             </div>
                             <div className="form-group">
-                                <input defaultValue={this.props.userEditOject.phone} type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" name="phone" />
+                                <input onChange={(event)=>this.isChange(event)} defaultValue={this.props.userEditOject.phone} type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" name="phone" />
                             </div>
                             <div className="form-group">
-                                <select defaultValue={this.props.userEditOject.permission} className="custom-select" required name="permission" onChange={(event)=>this.isChange(event)}>
+                                <select onChange={(event)=>this.isChange(event)} defaultValue={this.props.userEditOject.permission} className="custom-select" required name="permission" onChange={(event)=>this.isChange(event)}>
                                     <option value={1}>Admin</option>
                                     <option value={2}>Moderator</option>
                                     <option value={3}>Normal</option>
