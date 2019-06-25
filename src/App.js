2786d401ad81e84446f2b9b34bd8a0ee23da2653
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-25 11:47:15 +0700

    [PHAN-34] 100 Cach viet tat ghi de va them moi toan tu 3 cham

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ class App extends Component {
     // console.log(p2);
     // console.log(p1);
 
-    var a1 = [1,2,3,4];
-    var a2 = [...a1];
-    console.log(a1);
-    a2[0]=200;
-    console.log(a1);
-    console.log(a2);
+    // var a1 = [1,2,3,4];
+    // var a2 = [...a1];
+    // console.log(a1);
+    // a2[0]=200;
+    // console.log(a1);
+    // console.log(a2);
+
+    var b1 = {
+      num : [34,56,23],
+      status : true
+    };
+    var b2 = {...b1,status:false};
+    console.log(b2);
+    var b3 = {...b1,num:[...b1.num,100]};
+    console.log(b3);
     
     return (
       <div>
