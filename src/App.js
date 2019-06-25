dd62fea775f4f6a37f2e3cdd0dd6604cc908bad4
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-25 14:06:03 +0700

    [PHAN-36] 104 Thuc hanh voi redux store voi chi thi xoa bang filter

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ function App() {
         return {...state,editStatus:!state.editStatus};
       case "ADD_NEW":
         return {...state,num:[...state.num,action.newItem]};
+      case "DELETE":
+        return {...state,num:state.num.filter((value,key) => key !== action.index )};
       default:
-        break;
+        return state;
     }
-    return state;
+    
   }
   var store1 = redux.createStore(reducer1);
   store1.dispatch({type:"CHANGE_EDIT_STATUS"});
@@ -25,6 +27,11 @@ function App() {
   store1.dispatch({
     type:"ADD_NEW",
     newItem:'tai nghe'
+  });
+  console.log(store1.getState());
+  store1.dispatch({
+    type:"DELETE",
+    index:0
   });
   console.log(store1.getState());
 
