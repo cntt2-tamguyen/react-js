c42b008bc2737037cb199695178b5bea39228cfd
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-25 11:37:52 +0700

    [PHAN-34] 099 Su dung toan tu ba cham

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // var p1 = {
+    //   ten:"man hinh",
+    //   gia:90
+    // }
+    // console.log(p1);
+    // var p2 = {...p1};
+    // p2.gia = 100;
+    // console.log(p2);
+    // console.log(p1);
+
+    var a1 = [1,2,3,4];
+    var a2 = [...a1];
+    console.log(a1);
+    a2[0]=200;
+    console.log(a1);
+    console.log(a2);
+    
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
