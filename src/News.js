bbd72618a2778c44dbc585b0ccb8b72804545f4a
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-25 19:32:01 +0700

    [PHAN-38] 110 Cach goi ham store tu component con

--- src/News.js
+++ src/News.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class News extends Component {
+    userEditStatusInStore  = () => {
+        var {dispatch} = this.props;
+        dispatch({
+            type:"CHANGE_EDIT_STATUS"
+        })
+    }
     render() {
         return (
             <div>
                 <h2>Day la component News</h2>
+                <button onClick={() =>this.userEditStatusInStore()}>Click</button>
             </div>
         );
     }
 }
-
-export default News;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        editStatus:state.editStatus
+    }
+}
+export default connect(mapStateToProps)(News);
