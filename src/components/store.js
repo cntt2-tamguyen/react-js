62db0ecaf9be73229aee17d8879d0fe2eab6c632
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-27 11:48:01 +0700

    [PHAN-42] 125 Cai dat redux khoi tao store khoi tao reducer

---
+++ src/components/store.js
@@ -0,0 +1,13 @@
+import { noteData } from '/fib';
+
+var redux = require('redux');
+var store = redux.createStore(allReducer);
+const noteInitialState = {}
+const allReducer = (state = noteInitialState, action) => {
+    switch (action.type) {
+        case "ADD_DATA":
+            return state
+        default:
+            return state
+    }
+}
