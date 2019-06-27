5072a038f83ded8571ab6f6f6ee663227c8fe901
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-27 10:32:01 +0700

    [PHAN-41] 120 Them du lieu vao firebase

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { firebaseConnect } from './firebaseConnect';
+import * as  firebase from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+  pushData  = () => {
+    var connectData = firebase.database().ref('dataForNote');
+    connectData.push({
+      titleNote:'Tiêu đề ghi chú số 3',
+      contentNote:'Nội dung ghi chú số 3'
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <button onClick={() =>this.pushData()}>Click</button>
+      </div>
+    );
+  }
 }
 
 export default App;
