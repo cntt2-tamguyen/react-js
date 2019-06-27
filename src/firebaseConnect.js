bca5ac8f148b2d5fbaf3ce996b4e8d599144b16d
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-27 09:59:16 +0700

    [PHAN-41] 117 Ket noi react js voi firebase

---
+++ src/firebaseConnect.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyBydpaOYuj2hLoYUO39uUcGFJOwaNKQtCY",
+    authDomain: "notereactfedu-1f412.firebaseapp.com",
+    databaseURL: "https://notereactfedu-1f412.firebaseio.com",
+    projectId: "notereactfedu-1f412",
+    storageBucket: "",
+    messagingSenderId: "656045350839",
+    appId: "1:656045350839:web:28b4b259805ccea8"
+  };
+  // Initialize Firebase
+
+  export const firebaseConnect = firebase.initializeApp(firebaseConfig);
