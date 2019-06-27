f3037e8864921704c987f8259e74ae4c3ecc3689
cntt2-tamguyen <nhantam181@gmail.com>
2019-06-27 11:27:44 +0700

    [PHAN-42] 123 Buoc 2 click vao nut lay duoc du lieu

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 
 class NoteForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteTitle:'',
+            noteContent:''
+        }
+    }
+
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]:value
+        });
+    }
+    
+    addData  = (title,content) => {
+        var item = {};
+        item.title=title;
+        item.content=content;
+        console.log(item)
+    }
+
     render() {
         return (
             <div className="col-4">
                 <h3>Edit Note</h3>
-                <div className="form-group">
-                    <label >Tiêu đề Note</label>
-                    <input type="text" className="form-control" name="noteTitle" id="noteTitle" aria-describedby="helpIdNoteTitle" placeholder="Tiêu đề Note" />
-                    <small id="helpIdNoteTitle" className="form-text text-muted">Điền tiêu đề vào đây</small>
-                </div>
-                <div className="form-group">
-                    <label >Nội dung Note</label>
-                    <textarea type="text" className="form-control" name="noteContent" id="noteContent" aria-describedby="helpIdNoteContent" placeholder="Nội dung Note" defaultValue={""} />
-                    <small id="helpIdNoteContent" className="form-text text-muted">Điền nội dung vào đây</small>
-                </div>
-                <button type="submit" className="btn btn-primary btn-block">Save</button>
+                <form>
+                    <div className="form-group">
+                        <label >Tiêu đề Note</label>
+                        <input onChange={(event) =>this.isChange(event)} type="text" className="form-control" name="noteTitle" id="noteTitle" aria-describedby="helpIdNoteTitle" placeholder="Tiêu đề Note" />
+                        <small id="helpIdNoteTitle" className="form-text text-muted">Điền tiêu đề vào đây</small>
+                    </div>
+                    <div className="form-group">
+                        <label >Nội dung Note</label>
+                        <textarea onChange={(event) =>this.isChange(event)} type="text" className="form-control" name="noteContent" id="noteContent" aria-describedby="helpIdNoteContent" placeholder="Nội dung Note" defaultValue={""} />
+                        <small id="helpIdNoteContent" className="form-text text-muted">Điền nội dung vào đây</small>
+                    </div>
+                    <button type="reset" onClick={()=>this.addData(this.state.noteTitle,this.state.noteContent)} className="btn btn-primary btn-block">Save</button>
+                </form>
             </div>
         );
     }
